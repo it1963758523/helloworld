baabf4356f209d3ae8f8ea2a50ec29ab19400875
it1963758523 <1963758523@qq.com>
2019-07-26 18:15:20 +0800

    一次测试

---
+++ helloworld_git/src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author 黄开文
+ * @Version v 1.0
+ * @Date 2019/7/26
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello  world!!!");
+    }
+}
